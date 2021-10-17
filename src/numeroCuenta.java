541e83e12439b100c584ce3a4ec50ea6cb8b16c1
Lucas R. Luque <lucasrluque@icloud.com>
2021-10-17 17:08:11 +0200

    Añadidas nuevas funcionalidades

--- src/numeroCuenta.java
+++ src/numeroCuenta.java
@@ -1,29 +1,27 @@
-import java.util.Arrays;
 import java.util.Random;
 
 public class numeroCuenta {
-    public numeroCuenta(){
+    public String obtenerNumeroCuenta(){
         Random random = new Random();
-        int[] NC = new int[10];
-        for(int i = 0; i < NC.length; i++){
-            NC[i] = random.nextInt();
+        StringBuilder NC = new StringBuilder();;
+        for(int i = 0; i < 10; i++){
+            NC.append(random.nextInt(9));
         }
         int c1 = 0;
-        int[] CE = {9,0,1,0};
-        int[] CS = {0,2,0,1};
-        int a = 11 - ((6*CE[4]+3*CE[3]+7*CE[2]+9*CE[1])%11);
+        String CE = "9010";
+        String CS = "0201";
+        int a = 11 - ((6* (int) CE.charAt(3) +3* (int) CE.charAt(2)+7*(int)CE.charAt(1)+9*(int)CE.charAt(0))%11);
         if(a < 10){c1 = a;}
         else if (a == 10){c1 = 1;}
         else if(a == 11){c1 = 0;}
 
         int c2 = 0;
 
-        int b = 11 - ((NC[1] + 2*NC[2] + 4*NC[3] + 8*NC[4] + 5*NC[5] + 10*NC[6] + 9*NC[7] + 7*NC[8] + 3*NC[9])%11);
+        int b = 11 - ((NC.charAt(1) + 2*NC.charAt(2) + 4*NC.charAt(3) + 8*NC.charAt(4) + 5*NC.charAt(5)
+                + 10*NC.charAt(6) + 9*NC.charAt(7) + 7*NC.charAt(8) + 3*NC.charAt(9))%11);
         if(b < 10){c2 = b;}
         else if (b == 10){c2 = 1;}
         else if(b== 11){c2 = 0;}
-
-        System.out.println(Arrays.toString(CE) + Arrays.toString(CS) + Integer.toString(c1) + Integer.toString(c2) + Arrays.toString(NC));
-
+        return (CE + CS + c1 + c2 + NC);
     }
 }
--- src/todosLosMenus.java
+++ src/todosLosMenus.java
@@ -2,11 +2,18 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class todosLosMenus {
 
-    static String dniIntro; //Lo necesitamos en distintas funciones
+    static double saldo = 0;
+    static String numero;
+    static StringBuilder transacciones = new StringBuilder();
+    static StringBuilder datosCliente = new StringBuilder();
+    static boolean dadoAlta = false;
+    static boolean cuentaCreada = false;
 
     public static void menuInicial() throws IOException {
         Scanner scan = new Scanner(System.in);
@@ -18,23 +25,53 @@ public class todosLosMenus {
                 error. Esto evita que el programa se detenga al introducir texto en un nextInt() .
              */
 
-            System.out.print("\nIntroduzca la operación que desea realizar:\n\t1)Darse de alta\n\t2)Crear cuenta\n\t0)Salir\n\n");
-            String opcion = scan.nextLine();
-            try {
-                int numero = Integer.parseInt(opcion);
-                if (numero == 1) {
-                    stayMenu = false;
-                    darseDeAlta();
-                }
-                if (numero == 2)
-                    crearCuentaBancaria();
-                stayMenu = false;
+            System.out.print("""
 
-                if (numero == 0) {
+                    Introduzca la operación que desea realizar:
+                    \t1)Darse de alta
+                    \t2)Crear cuenta
+                    \t3)Realizar depósito
+                    \t4)Realizar extracción
+                    \t5)Datos de cliente
+                    \t0)Salir
+
+                    """);
+            String opcion = scan.nextLine();
+            if (opcion.equals("1")) {
+                stayMenu = false;
+                darseDeAlta();
+            } else if (opcion.equals("2")) {
+                if (dadoAlta) {
+                    crearCuentaBancaria();
                     stayMenu = false;
+                } else {
+                    System.out.println("Debe darse de alta.");
                 }
-            } catch(Exception e){
-                System.out.println("Carácter inválido");
+            } else if (opcion.equals("3")) {
+                if(cuentaCreada) {
+                    realizarDeposito();
+                    stayMenu = false;
+                }else{
+                    System.out.println("Primero debe crear una cuenta");
+                }
+            } else if (opcion.equals("4")) {
+                if (cuentaCreada) {
+                    realizarExtraccion();
+                    stayMenu = false;
+                } else {
+                    System.out.println("Primero debe crear una cuenta");
+                }
+            } else if (opcion.equals("5")) {
+                if (dadoAlta) {
+                    consultaDatos();
+                    stayMenu = false;
+                } else {
+                    System.out.println("Debe darse de alta.");
+                }
+            } else if (opcion.equals("0")) {
+                stayMenu = false;
+            } else {
+                System.out.println("Opción inválida");
             }
         }
     }
@@ -48,98 +85,248 @@ public class todosLosMenus {
         boolean correoCorrecto = false;
         boolean fechaCorrecta = false;
 
-        String nombre, dni, correo, primerApellido, segundoApellido, mes, dia, anio;
+        String nombre, dni = null, correo = null, primerApellido, segundoApellido, mes, dia, anio, nombreCompleto = null,
+                fechaFinal = null;
 
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Nombre: ");
-        nombre = scan.nextLine();
+        System.out.println("Darse de alta\nPulse ENTER para continuar o '0' para volver.");
+        String volver = scan.nextLine();
+        if (volver.equals("0")) {
+            menuInicial();
+        } else {
 
-        System.out.println("Primer apellido: ");
-        primerApellido = scan.nextLine();
+            System.out.println("Nombre: ");
+            nombre = scan.nextLine();
 
-        System.out.println("Segundo apellido: ");
-        segundoApellido = scan.nextLine();
+            System.out.println("Primer apellido: ");
+            primerApellido = scan.nextLine();
 
-        System.out.println("DNI: ");
-        do {
-            dni = scan.nextLine();
-            if (new validarDNI().validarNumero(dni) && new validarDNI().validarLetra(dni)) {
-                dniCorrecto = true;
-                //Tienen que devolver true los dos metodos de la clase validar DNI
-            } else {
-                System.out.println("DNI incorrecto. Intente de nuevo por favor.");
-            }
-        } while (!dniCorrecto);
+            System.out.println("Segundo apellido: ");
+            segundoApellido = scan.nextLine();
 
-        System.out.println("Correo electrónico: ");
-        do {
-            correo = scan.nextLine();
-            if (correo.endsWith("@alumnos.upm.es") || correo.endsWith("@upm.es")) {
-                correoCorrecto = true;
-            } else {
-                System.out.println("Correo incorrecto. Intente de nuevo.");
-            }
-        } while (!correoCorrecto);
+            nombreCompleto = nombre + " " + primerApellido + " " + segundoApellido;
 
-        System.out.println("Introduzca su fecha de nacimiento en formato dd/mm/aaaa");
-        do {
-            System.out.println("Dia: ");
-            dia = scan.nextLine();
+            System.out.println("DNI: ");
+            do {
+                dni = scan.nextLine();
+                if (new validarDNI().validarNumero(dni) && new validarDNI().validarLetra(dni)) {
+                    dniCorrecto = true;
+                    //Tienen que devolver true los dos metodos de la clase validar DNI
+                } else {
+                    System.out.println("DNI incorrecto. Intente de nuevo por favor.");
+                }
+            } while (!dniCorrecto);
 
-            System.out.println("Mes: ");
-            mes = scan.nextLine();
+            System.out.println("Correo electrónico: ");
+            do {
+                correo = scan.nextLine();
+                if (correo.endsWith("@alumnos.upm.es") || correo.endsWith("@upm.es")) {
+                    correoCorrecto = true;
+                } else {
+                    System.out.println("Correo incorrecto. Intente de nuevo.");
+                }
+            } while (!correoCorrecto);
 
-            System.out.println("Año: ");
-            anio = scan.nextLine();
+            System.out.println("Introduzca su fecha de nacimiento en formato dd/mm/aaaa");
+            do {
+                System.out.println("Dia: ");
+                dia = scan.nextLine();
 
-            if (validarFecha.validar(dia, mes, anio)) {
-                fechaCorrecta = true;
-            }
-        } while (!fechaCorrecta);
+                System.out.println("Mes: ");
+                mes = scan.nextLine();
 
+                System.out.println("Año: ");
+                anio = scan.nextLine();
+
+                fechaFinal = dia + "/" + mes + "/" + anio;
+
+                if (validarFecha.validar(dia, mes, anio)) {
+                    fechaCorrecta = true;
+                }
+
+                dadoAlta = true;
+
+            } while (!fechaCorrecta);
+        }
+
+        datosCliente.append("\n" + nombreCompleto + "\n" + dni + "\n" + correo + "\n" + fechaFinal + "\nCuentas: \n\t");
         menuInicial();
     }
 
-    public static void crearCuentaBancaria() {
-
+    public static void crearCuentaBancaria() throws IOException {
         Scanner scan = new Scanner(System.in);
         upmBankAscii.logo();
-        boolean cuentaCorriente, cuentaAhorro, cuentaRemunerada = false;
-        boolean menuCuenta = true;
-        while(menuCuenta) {
-            String nombre, primerApellido, segundoApellido, nombreCompleto;
-            System.out.println("Escriba su nombre");
-            nombre = scan.nextLine() + " ";
-            System.out.println("Escriba su primer apellido");
-            primerApellido = scan.nextLine() + " ";
-            System.out.println("Escriba su segundo apellido");
-            segundoApellido = scan.nextLine() + " ";
-            nombreCompleto = nombre + primerApellido + segundoApellido;
-            System.out.println("Elija su tipo de cuenta: \n1) Corriente\n2) Ahorro\n3) Remunerada");
-            int opcion = scan.nextInt();
-            try {
-                if (opcion == 1){
-                    cuentaCorriente = true;
-                    scan.close();
-                }else if(opcion == 2){
-                    cuentaAhorro = true;
-                    scan.close();
-                }else if(opcion == 3){
-                    cuentaRemunerada = true;
-                    scan.close();
+        System.out.println("Crear cuenta\nPulse ENTER para continuar o '0' para volver.");
+        String volver = scan.nextLine();
+        if (!volver.equals("0")) {
+
+            boolean cuentaAhorro = false, cuentaRemunerada = false, menuCuenta = true, menuOpcion = true;
+            String nombre, primerApellido, segundoApellido, nombreCompleto = null;
+            while (menuCuenta) {
+                System.out.println("Escriba su nombre");
+                nombre = scan.nextLine() + " ";
+                System.out.println("Escriba su primer apellido");
+                primerApellido = scan.nextLine() + " ";
+                System.out.println("Escriba su segundo apellido");
+                segundoApellido = scan.nextLine() + " ";
+                nombreCompleto = nombre + primerApellido + segundoApellido;
+                while (menuOpcion) {
+                    System.out.println("Elija su tipo de cuenta: \n1) Corriente\n2) Ahorro\n3) Remunerada");
+                    String opcion = scan.nextLine();
+                    if (opcion.equals("1")) {
+                        menuCuenta = false;
+                        menuOpcion = false;
+                    } else if (opcion.equals("2")) {
+                        cuentaAhorro = true;
+                        menuCuenta = false;
+                        menuOpcion = false;
+                    } else if (opcion.equals("3")) {
+                        cuentaRemunerada = true;
+                        menuCuenta = false;
+                        menuOpcion = false;
+                    } else {
+                        System.out.println("Opción inválida");
+                    }
                 }
-            }catch(Exception e){
-                System.out.println("Carácter inválido");
-        }
 
+                cuentaCreada = true;
+
+            }
+
+            numero = new numeroCuenta().obtenerNumeroCuenta();
             System.out.println(nombreCompleto);
-            if(cuentaAhorro = true){ System.out.println("Cuenta de ahorro");}
-            else if(cuentaRemunerada = true){ System.out.println("Cuenta remunerada");}
-            else if(cuentaCorriente = true){ System.out.println("Cuenta corriente");}
-            numeroCuenta numeroCuenta = new numeroCuenta();
-            menuCuenta = false;
+            if (cuentaAhorro) {
+                datosCliente.append(numero + " Cuenta ahorro\t");
+                System.out.println("Cuenta de ahorro");
+            } else if (cuentaRemunerada) {
+                datosCliente.append(numero + " Cuenta remunerada\t");
+                System.out.println("Cuenta remunerada");
+            } else {
+                datosCliente.append(numero + " Cuenta corriente\t");
+                System.out.println("Cuenta corriente");
+            }
+            System.out.println("Número de cuenta: " + numero);
+            System.out.println("Saldo: " + saldo + "€");
+            System.out.println("Pulse ENTER para continuar");
+            scan.nextLine();
+        }
+        menuInicial();
+    }
 
+
+    public static void realizarDeposito() throws IOException {
+        upmBankAscii.logo();
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Realizar depósito\nPulse ENTER para continuar o '0' para volver.");
+        String volver = scan.nextLine();
+        boolean menuPrincipal = true, menuIngreso = true;
+        if (volver.equals("0")) {
+            menuInicial();
+        } else {
+            while (menuPrincipal) {
+                System.out.println("Escriba la cuenta a la que desea realizar un depósito: ");
+                String cuentaObjetivo = scan.nextLine();
+                if (cuentaObjetivo.equals(numero)) {
+                    while (menuIngreso) {
+                        System.out.println("Escriba la cantidad de dinero que desea transferir: ");
+                        try {
+                            String ingreso = scan.nextLine();
+                            double ingresoNum = Double.parseDouble(ingreso);
+                            if (ingresoNum > 0) {
+                                saldo += ingresoNum;
+                                System.out.println("Ingreso realizado con éxito.");
+                                System.out.println("[" + numero + "] Saldo de la cuenta: " + saldo + "€");
+                                transacciones.append("\n[" + numero + "] " + "+" + ingresoNum + "€ " + "Saldo: " + saldo + "€");
+                                menuIngreso = false;
+                                menuPrincipal = false;
+                                System.out.println("Pulse ENTER para continuar");
+                                scan.nextLine();
+                                menuInicial();
+                            } else {
+                                System.out.println("Cifra inválida, intente de nuevo.");
+                            }
+                        } catch (Exception e) {
+                            System.out.println("Cifra inválida, intente de nuevo.");
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+
+    public static void realizarExtraccion() throws IOException {
+        upmBankAscii.logo();
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Realizar extracción\nPulse ENTER para continuar o '0' para volver.");
+        String volver = scan.nextLine();
+        boolean menuPrincipal = true, menuIngreso = true;
+        if (volver.equals("0")) {
+            menuInicial();
+        } else {
+            while (menuPrincipal) {
+                System.out.println("Escriba la cuenta a la que desea realizar una extracción: ");
+                String cuentaObjetivo = scan.nextLine();
+                if (cuentaObjetivo.equals(numero)) {
+                    while (menuIngreso) {
+                        System.out.println("Escriba la cantidad de dinero que desea extraer: ");
+                        try {
+                            String extraccion = scan.nextLine();
+                            double extrNum = Double.parseDouble(extraccion);
+                            if(extrNum > saldo){
+                                System.out.println("Fondos insuficientes");
+                            }else {
+                                if (extrNum > 0) {
+                                    saldo -= extrNum;
+                                    System.out.println("Extracción realizada con éxito.");
+                                    System.out.println("[" + numero + "] Saldo de la cuenta: " + saldo + "€");
+                                    transacciones.append("\n[" + numero + "] " + "-" + extrNum + "€ " + "Saldo: " + saldo + "€");
+                                    menuIngreso = false;
+                                    menuPrincipal = false;
+                                    System.out.println("Pulse ENTER para continuar");
+                                    scan.nextLine();
+                                    menuInicial();
+                                } else {
+                                    System.out.println("Cifra inválida, intente de nuevo.");
+                                }
+                            }
+                        } catch (Exception e) {
+                            System.out.println("Cifra inválida, intente de nuevo.");
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+
+    public static void consultaDatos() throws IOException {
+        upmBankAscii.logo();
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Consultar datos de cliente\nPulse ENTER para continuar o '0' para volver.");
+        String volver = scan.nextLine();
+        boolean menu = true;
+        if (!volver.equals("0")) {
+            System.out.println(datosCliente);
+            while (menu) {
+                System.out.println("\n1)Ver transacciones\n2)Salir");
+                String opcion = scan.nextLine();
+                if (opcion.equals("1")) {
+                    System.out.println(transacciones);
+                    System.out.println("\n Pulse ENTER para continuar");
+                    scan.nextLine();
+                    menu = false;
+                    menuInicial();
+                } else if (opcion.equals("2")) {
+                    menu = false;
+                    menuInicial();
+                } else {
+                    System.out.println("Opción inválida");
+                }
+            }
         }
     }
 }
